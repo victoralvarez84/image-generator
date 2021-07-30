9de4b0ee013244ca83cdcc1cf18baf4c9c13d015
Victor Alvarez <victoralvareztech@gmail.com>
2021-07-30 09:28:26 -0400

    code-challenge

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="app" class="content-container">
+    <Header title="Victor's Image Generator"/>
+    <p v-if="selectedImage" class="image-box">
+    <img style="width:100%; height:100%" :src="selectedImage" alt="">
+    </p>
+    <div class="button-box">
+     <button @click="generate()">Generate random image</button> 
+    </div>
+    </div>
+  
+</template>
+
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  name: 'App',
+  components:{
+    Header
+  },
+  data () {
+    return {
+      images: [
+      'https://picsum.photos/600/200',
+      'https://picsum.photos/600/200',
+      'https://picsum.photos/600/200'
+    ],
+       selectedImage: null
+    }
+  },
+  methods: {
+   randomItem (items) {
+    return items[Math.floor(Math.random()*items.length)];
+    },
+   generate () {
+    this.selectedImage = this.randomItem(this.images)
+    },
+   created() {
+    this.selectedImage = this.randomItem(this.images)
+  }
+}
+}
+</script>
+
+<style>
+
+body{
+margin:0;
+padding:0;
+box-sizing: border-box;
+}
+
+.content-container{
+display:flex;
+flex-direction: column;
+align-items:center;
+}
+
+.image-box{
+ width:75%;
+}
+
+.button-box{
+display:flex;
+justify-content: center;
+}
+
+button{
+  background-color:#101010;
+  border:none;
+  height:32px;
+  color:white;
+  padding:16px;
+  border-radius:16px;
+  display:flex;
+  justify-content: center;
+  align-items:center;
+  margin-top:48px;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+      <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+    props: ['title']
+}
+</script>
+
+<style Lang="scss" scoped>
+   header{
+       padding: 24px 32px;
+       display:flex;
+       justify-content: center;
+   }
+
+   h1{
+       font-size:32px;
+       font-weight:700;
+       text-align:center;
+       text-transform: uppercase;
+       font-family: Futura;
+   }
+
+</style>
